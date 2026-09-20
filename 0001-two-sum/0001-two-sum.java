ef3b4a3bb2f24b8f763ddf490caed49351461f72
Hubert Bala Joshwin D <133846933+HBJ17@users.noreply.github.com>
2026-09-20 22:51:12 +0530

    1_two_sum_java_hashmap

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (seen.containsKey(complement)) {
+                return new int[]{seen.get(complement), i};
+            }
+
+            seen.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
